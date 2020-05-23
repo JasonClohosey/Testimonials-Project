3edf8de230843022dd61d1d4a2489ddf912d723d
Jason Clohosey <clohoseyjason@gmail.com>
2020-05-23 17:00:49 -0400

    Made array for all reviewers and their info. Added photos. Tested array of Objects

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-alert("Review!!!");
+var review = [
+{
+    pic: "assets/images/photo-0.jpeg", 
+    name: "Julia", 
+    star: "5 Stars", 
+    comment: "In condimentum libero lorem, eget mattis lectus tempus et. In iaculis, nibh et rhoncus accumsan, turpis est pharetra libero, ac aliquam urna metus sit amet velit."
+},
+{
+    pic: "assets/images/photo-1.jpeg", 
+    name: "Kevin", 
+    star: "4.5 Stars", 
+    comment: "Pellentesque ac commodo elit, vel ultrices dolor. Aenean eget dolor tellus."
+},
+{
+    pic: "assets/images/photo-2.jpeg", 
+    name: "Jackson", 
+    star: "5 Stars", 
+    comment: "Morbi nisl sem, vehicula sit amet congue nec, commodo sit amet ante. Phasellus et vestibulum velit. Phasellus sit amet placerat velit."
+},    
+{   
+    pic: "assets/images/photo-3.jpeg", 
+    name: "James", 
+    star: "4.8 Stars", 
+    comment: "Aenean sed accumsan justo. Nulla porta augue non lacus laoreet, id ultricies felis vestibulum. Aenean eu fermentum odio."
+},
+{
+    pic: "assets/images/photo-4.jpeg", 
+    name: "Beth", 
+    star: "5 Stars", 
+    comment: "Pellentesque vestibulum id mi ac rhoncus. Vestibulum tristique mollis placerat. Fusce vel metus orci."
+}];
+
+
+console.log(review[0]);
