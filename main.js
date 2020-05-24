a113cf4b6737a1ae28379624d5e3610795fe7544
Jason Clohosey <clohoseyjason@gmail.com>
2020-05-23 22:27:34 -0400

    Finished Javascript and tested for functionality. Finished styling.

--- main.js
+++ main.js
@@ -1,34 +1,70 @@
 var review = [
 {
-    pic: "assets/images/photo-0.jpeg", 
+    image: "./images/photo-0.jpeg", 
     name: "Julia", 
     star: "5 Stars", 
     comment: "In condimentum libero lorem, eget mattis lectus tempus et. In iaculis, nibh et rhoncus accumsan, turpis est pharetra libero, ac aliquam urna metus sit amet velit."
 },
 {
-    pic: "assets/images/photo-1.jpeg", 
+    image: "./images/photo-1.jpeg", 
     name: "Kevin", 
     star: "4.5 Stars", 
     comment: "Pellentesque ac commodo elit, vel ultrices dolor. Aenean eget dolor tellus."
 },
 {
-    pic: "assets/images/photo-2.jpeg", 
+    image: "./images/photo-2.jpeg", 
     name: "Jackson", 
     star: "5 Stars", 
     comment: "Morbi nisl sem, vehicula sit amet congue nec, commodo sit amet ante. Phasellus et vestibulum velit. Phasellus sit amet placerat velit."
 },    
 {   
-    pic: "assets/images/photo-3.jpeg", 
+    image: "./images/photo-3.jpeg", 
     name: "James", 
     star: "4.8 Stars", 
     comment: "Aenean sed accumsan justo. Nulla porta augue non lacus laoreet, id ultricies felis vestibulum. Aenean eu fermentum odio."
 },
 {
-    pic: "assets/images/photo-4.jpeg", 
+    image: "./images/photo-4.jpeg", 
     name: "Beth", 
     star: "5 Stars", 
     comment: "Pellentesque vestibulum id mi ac rhoncus. Vestibulum tristique mollis placerat. Fusce vel metus orci."
 }];
 
+var reviewIndex = 0
+// var image = review.name;
+console.log("This is the reviewrs name", review[reviewIndex].name)
+console.log("This is the review image", review[reviewIndex].image);
 
-console.log(review[0]);
+const buttons = document.querySelectorAll(".btn");
+const imageDiv = document.querySelector(".image");
+
+changeReview = () => {
+    currentReview = review[reviewIndex];
+    console.log("Current review in change review function", currentReview)
+
+    // document.querySelector(".image").innerHTML = `url(${currentReview.image})`;
+    document.querySelector(".name").innerHTML = currentReview.name;
+    document.querySelector(".star").innerHTML = currentReview.star;
+    document.querySelector(".comment").innerHTML =
+    currentReview.comment;
+    imageDiv.style.backgroundImage = `url(${currentReview.image})`;
+}
+
+buttons.forEach((button) => {
+    button.addEventListener("click", function() {
+        if (button.classList.contains("backBtn")) {
+            reviewIndex--
+        if (reviewIndex < 0) {
+            reviewIndex = review.length - 1;
+        }    
+            changeReview()
+        }
+        if (button.classList.contains("forwardBtn")) {
+            reviewIndex++
+        if (reviewIndex > review.length -1) {
+            reviewIndex = 0;
+        }
+            changeReview()
+        }
+    });
+});
